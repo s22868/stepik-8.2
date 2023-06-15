b38f1b29f573b918f92dc686de9a7c7c2f5c1dc3
Mati <mati.leeder@gmail.com>
2023-06-15 17:43:38 +0200

    9.2.1

---
+++ 8.2.3/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react', 'jsx-a11y'],
+  rules: {},
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
