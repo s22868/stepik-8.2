8e959378303965150db20769d22ea2348e29dd07
Mati <mati.leeder@gmail.com>
2023-06-15 16:31:24 +0200

    8.2.2

--- 8.2.2/src/App.tsx
+++ 8.2.2/src/App.tsx
@@ -1,23 +1,52 @@
-import React from 'react';
+import * as React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useParams,
+  useSearchParams,
+} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Calculator = () => {
+  const { operation } = useParams()
+  const [searchParams] = useSearchParams()
+  const x = searchParams.get('x')
+  const y = searchParams.get('y')
+
+  if (x === null || y === null) {
+    return <div>Brak x lub y</div>
+  }
+
+  let result: number | string
+
+  switch (operation) {
+    case 'add':
+      result = Number(x) + Number(y)
+      break
+    case 'sub':
+      result = Number(x) - Number(y)
+      break
+    case 'mul':
+      result = Number(x) * Number(y)
+      break
+    case 'div':
+      result = Number(x) / Number(y)
+      break
+    default:
+      result = 'tylko /mul /div /add /sub'
+  }
+
+  return <h1>Wynik: {result}</h1>
 }
 
-export default App;
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/:operation" Component={Calculator} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
